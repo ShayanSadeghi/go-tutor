5ddce33da8a5564e04ad62e544dbbbebfddae1a9
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2024-03-22 12:29:38 +0330

    using slices and extending ticketing app logic

--- main.go
+++ main.go
@@ -5,7 +5,8 @@ import "fmt"
 func main() {
 	const conferenceName = "GO conference"
 	const totalTickets = 50
-	var remainingTickets = 50
+	var remainingTickets uint = 50
+	var bookings []string
 
 	fmt.Printf("the type of conferenceName is %T, the type of the totaltickets is %T\n", conferenceName, totalTickets)
 
@@ -13,11 +14,32 @@ func main() {
 	fmt.Printf("There is total %v tickets and %v are still available\n", totalTickets, remainingTickets)
 	fmt.Println("Enjoy the conference!")
 
-	var userName string
+	var firstName string
+	var lastName string
+	var email string
 	var userTickets uint
 
-	fmt.Scan(&userName)
+	fmt.Println("Enter your first name")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your last name")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email address")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter the ticket numbers you want to book")
 	fmt.Scan(&userTickets)
 
-	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+	bookings = append(bookings, firstName+" "+lastName)
+
+	fmt.Printf("Thank you %v %v. You booked %v tickets. your tickets send to your confirmed email %v\n", firstName, lastName, userTickets, email)
+
+	remainingTickets = remainingTickets - userTickets
+	fmt.Printf("%v tickets reamins", remainingTickets)
+
+	fmt.Printf("Bookings values %v\n", bookings)
+	fmt.Printf("Bookings type is %T\n", bookings)
+	fmt.Printf("Bookings length %v\n", len(bookings))
+
 }
