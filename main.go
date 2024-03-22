d62843c7b8227ce5c9b48407a82ee51751059191
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2024-03-22 12:17:19 +0330

    data types, input data

--- main.go
+++ main.go
@@ -7,7 +7,17 @@ func main() {
 	const totalTickets = 50
 	var remainingTickets = 50
 
+	fmt.Printf("the type of conferenceName is %T, the type of the totaltickets is %T\n", conferenceName, totalTickets)
+
 	fmt.Printf("Hello and welcome to %v. Here you can buy tickets\n", conferenceName)
 	fmt.Printf("There is total %v tickets and %v are still available\n", totalTickets, remainingTickets)
 	fmt.Println("Enjoy the conference!")
+
+	var userName string
+	var userTickets uint
+
+	fmt.Scan(&userName)
+	fmt.Scan(&userTickets)
+
+	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
 }
