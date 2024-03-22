60856aad0bd89842a40917cff2e7be8eaac371fc
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2024-03-22 17:54:30 +0330

    keep code running until finishing the code

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"booking-app/validator"
 	"fmt"
+	"sync"
 	"time"
 )
 
@@ -13,6 +14,8 @@ type UserData struct {
 	countOfTickets uint
 }
 
+var wg = sync.WaitGroup{} // we use this to prevent code exiting before all threads done
+
 func main() {
 
 	conferenceName := "GO conference" //declare and assign
@@ -22,47 +25,51 @@ func main() {
 
 	greetingUser(conferenceName, totalTickets, remainingTickets)
 
-	for {
+	// for {
 
-		firstName, lastName, email, userTickets := getUserData()
-		isValidUserData := validator.ValidateData(firstName, lastName, email)
-		isValidTicketNumber := validator.ValidateTickets(remainingTickets, userTickets)
+	firstName, lastName, email, userTickets := getUserData()
+	isValidUserData := validator.ValidateData(firstName, lastName, email)
+	isValidTicketNumber := validator.ValidateTickets(remainingTickets, userTickets)
 
-		if !isValidUserData || !isValidTicketNumber {
-			continue
-		}
-
-		var userData = UserData{
-			firstName:      firstName,
-			lastName:       lastName,
-			email:          email,
-			countOfTickets: userTickets,
-		}
-
-		bookings = append(bookings, userData)
-
-		go sendTicket(userTickets, firstName, lastName, email)
-
-		fmt.Printf("Thank you %v %v. You booked %v tickets. your tickets send to your confirmed email %v\n", firstName, lastName, userTickets, email)
-
-		remainingTickets = remainingTickets - userTickets
-		fmt.Printf("%v tickets reamins", remainingTickets)
-
-		var firstNames []string = getFirstNames(bookings)
-
-		fmt.Printf("The list of bookings is: %v\n", firstNames)
-
-		fmt.Printf("The bookings are: %v\n", bookings)
-
+	if !isValidUserData || !isValidTicketNumber {
+		fmt.Println("There is an error in user data")
+		// continue
 	}
+
+	var userData = UserData{
+		firstName:      firstName,
+		lastName:       lastName,
+		email:          email,
+		countOfTickets: userTickets,
+	}
+
+	bookings = append(bookings, userData)
+
+	wg.Add(1) // as we create a new thread in the following line, we add 1 to the wg counter
+	go sendTicket(userTickets, firstName, lastName, email)
+
+	fmt.Printf("Thank you %v %v. You booked %v tickets. your tickets send to your confirmed email %v\n", firstName, lastName, userTickets, email)
+
+	remainingTickets = remainingTickets - userTickets
+	fmt.Printf("%v tickets reamins", remainingTickets)
+
+	var firstNames []string = getFirstNames(bookings)
+
+	fmt.Printf("The list of bookings is: %v\n", firstNames)
+
+	fmt.Printf("The bookings are: %v\n", bookings)
+
+	wg.Wait() // wait until the counter become 0
+	// }
 }
 
 func sendTicket(ticketsNumber uint, firstName string, lastName string, email string) {
-	time.Sleep(30 * time.Second) // this process takes time to finish
+	time.Sleep(10 * time.Second) // this process takes time to finish
 
 	var ticketData = fmt.Sprintf("%v tickets booked for %v %v\n", ticketsNumber, firstName, lastName) // save formatted text in strting variable
 
 	fmt.Println("###########")
 	fmt.Printf("Ticket\n%v\nis sending to %v\n", ticketData, email)
 	fmt.Println("###########")
+	wg.Done() // decrease 1 from the wg counter
 }
